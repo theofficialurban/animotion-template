698e16ff6366f09bcb3be025b2c7b0a5c3c893c5
Urban <josh@misterslandscaping.com>
2024-03-22 15:29:01 -0400

    Initial

---
+++ src/slides.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Presentation, Slide, Code } from '@components'
+	import { signal } from '@motion'
+
+	const circle = signal(
+		{ x: 0, y: 200, r: 80, fill: '#00ffff' },
+		{ duration: 2000 }
+	)
+</script>
+
+<Presentation>
+	<Slide animate>
+		<p class="font-bold text-8xl">🪄 Animotion</p>
+	</Slide>
+</Presentation>
